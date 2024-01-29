310d03ed59466395736328cebe119ce5822a4d99
lucap46 <158043363+lucap46@users.noreply.github.com>
2024-01-29 15:47:32 +0000

    luca finocchio

---
+++ lunedi2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int numero;
+    int numero1;
+    int numero2;
+    int numero3;8======D
+    
+    scanf( "%d" "%d", &numero, &numero);
+    printf("il tuo risultato è: %d\n" , numero + numero);
+
+    return (0);
+}
